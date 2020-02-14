ccbdc6cfd95789b46976744addad5557b61eb8db
Leo <losoviz@gmail.com>
2020-02-14 15:57:58 +0800

    moved logic to JS file, moved other JS files to vendors/

---
+++ assets/js/graphiql-client.js
@@ -0,0 +1,122 @@
+// Parse the search string to get url parameters.
+var search = window.location.search;
+var parameters = {};
+search
+  .substr(1)
+  .split('&')
+  .forEach(function(entry) {
+    var eq = entry.indexOf('=');
+    if (eq >= 0) {
+      parameters[decodeURIComponent(entry.slice(0, eq))] = decodeURIComponent(
+        entry.slice(eq + 1),
+      );
+    }
+  });
+
+// When the query and variables string is edited, update the URL bar so
+// that it can be easily shared.
+function onEditQuery(newQuery) {
+  parameters.query = newQuery;
+  updateURL();
+}
+
+function onEditVariables(newVariables) {
+  parameters.variables = newVariables;
+  updateURL();
+}
+
+function onEditOperationName(newOperationName) {
+  parameters.operationName = newOperationName;
+  updateURL();
+}
+
+function updateURL() {
+  var newSearch =
+    '?' +
+    Object.keys(parameters)
+      .filter(function(key) {
+        return Boolean(parameters[key]);
+      })
+      .map(function(key) {
+        return (
+          encodeURIComponent(key) + '=' + encodeURIComponent(parameters[key])
+        );
+      })
+      .join('&');
+  history.replaceState(null, null, newSearch);
+}
+
+/*
+ * Converts a string to a bool.
+ *
+ * This conversion will:
+ *
+ *  - match 'true', 'on', or '1' as true.
+ *  - ignore all white-space padding
+ *  - ignore capitalization (case).
+ *
+ * '  tRue  ','ON', and '1   ' will all evaluate as true.
+ *
+ * Taken from https://stackoverflow.com/a/264180
+ *
+ */
+function strToBool(s)
+{
+    // will match one and only one of the string 'true','1', or 'on' rerardless
+    // of capitalization and regardless off surrounding white-space.
+    //
+    regex=/^\s*(true|1|on)\s*$/i
+    return regex.test(s);
+}
+
+// Defines a GraphQL fetcher using the fetch API. You're not required to
+// use fetch, and could instead implement graphQLFetcher however you like,
+// as long as it returns a Promise or Observable.
+function graphQLFetcher(graphQLParams) {
+  let nonce = (window.graphQLByPoPGraphiQLSettings && window.graphQLByPoPGraphiQLSettings.nonce) ? window.graphQLByPoPGraphiQLSettings.nonce : null;
+  let apiURL = (window.graphQLByPoPGraphiQLSettings && window.graphQLByPoPGraphiQLSettings.endpoint) ? window.graphQLByPoPGraphiQLSettings.endpoint : window.location.origin;
+
+  // Copy parameters
+  if (parameters.use_namespace && strToBool(parameters.use_namespace)) {
+    apiURL += '&use_namespace=true';
+  }
+  return fetch(apiURL, {
+    method: 'post',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      'X-WP-Nonce': nonce
+    },
+    body: JSON.stringify(graphQLParams),
+    credentials: 'include',
+  })
+    .then(function(response) {
+      return response.text();
+    })
+    .then(function(responseBody) {
+      try {
+        return JSON.parse(responseBody);
+      } catch (error) {
+        return responseBody;
+      }
+    });
+}
+
+// Render <GraphiQL /> into the body.
+// See the README in the top level of this module to learn more about
+// how you can customize GraphiQL by providing different values or
+// additional child elements.
+ReactDOM.render(
+  React.createElement(GraphiQL, {
+    fetcher: graphQLFetcher,
+    query: parameters.query,
+    variables: parameters.variables,
+    operationName: parameters.operationName,
+    onEditQuery: onEditQuery,
+    onEditVariables: onEditVariables,
+    // defaultVariableEditorOpen: true,
+    // docExplorerOpen: true,
+    onEditOperationName: onEditOperationName
+  }),
+  document.getElementById('graphiql'),
+);
--- src/Plugin.php
+++ src/Plugin.php
@@ -25,13 +25,7 @@ class Plugin {
     {
         $currentScreen = get_current_screen()->id;
 		if (strpos($currentScreen, 'graphql_by_pop') !== false) {
-			wp_enqueue_style(
-                'graphql-by-pop-graphiql',
-                GRAPHQL_BY_POP_PLUGIN_URL.'assets/css/graphiql.min.css',
-                array(),
-                false,
-                false
-            );
+            // CSS
 			wp_enqueue_style(
                 'graphql-by-pop-graphiql-client',
                 GRAPHQL_BY_POP_PLUGIN_URL.'assets/css/graphiql-client.css',
@@ -39,30 +33,46 @@ class Plugin {
                 false,
                 false
             );
-			wp_enqueue_script(
-                'graphql-by-pop-react',
-                GRAPHQL_BY_POP_PLUGIN_URL.'assets/js/react.development.js',
+			wp_enqueue_style(
+                'graphql-by-pop-graphiql',
+                GRAPHQL_BY_POP_PLUGIN_URL.'assets/css/vendors/graphiql.min.css',
                 array(),
                 false,
                 false
             );
+
+            // JS: execute them all in the footer
+			wp_enqueue_script(
+                'graphql-by-pop-react',
+                GRAPHQL_BY_POP_PLUGIN_URL.'assets/js/vendors/react.development.js',
+                array(),
+                false,
+                true
+            );
 			wp_enqueue_script(
                 'graphql-by-pop-react-dom',
-                GRAPHQL_BY_POP_PLUGIN_URL.'assets/js/react-dom.development.js',
+                GRAPHQL_BY_POP_PLUGIN_URL.'assets/js/vendors/react-dom.development.js',
                 array('graphql-by-pop-react'),
                 false,
-                false
+                true
             );
 			wp_enqueue_script(
                 'graphql-by-pop-graphiql',
-                GRAPHQL_BY_POP_PLUGIN_URL.'assets/js/graphiql.min.js',
+                GRAPHQL_BY_POP_PLUGIN_URL.'assets/js/vendors/graphiql.min.js',
                 array('graphql-by-pop-react-dom'),
                 false,
-                false
+                true
+            );
+			wp_enqueue_script(
+                'graphql-by-pop-graphiql-client',
+                GRAPHQL_BY_POP_PLUGIN_URL.'assets/js/graphiql-client.js',
+                array('graphql-by-pop-graphiql'),
+                false,
+                true
             );
 
 			wp_localize_script(
-				'graphql-by-pop-graphiql',
+				'graphql-by-pop-graphiql-client',
 				'graphQLByPoPGraphiQLSettings',
 				array(
 					'nonce' => wp_create_nonce('wp_rest'),
