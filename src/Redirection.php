0c83bfe8d974bd23b230c0a4af8ac96da99ddb93
Leo <losoviz@gmail.com>
2020-02-14 22:48:38 +0800

    Include files instead of redirecting

--- src/Redirection.php
+++ src/Redirection.php
@@ -25,15 +25,46 @@ class Redirection {
         );
     }
 
+    // public function parseRequest()
+    // {
+    //     $redirectPaths = [
+    //         trim($this->GRAPHIQL_ENDPOINT, '/') => 'graphiql',
+    //         trim($this->VOYAGER_ENDPOINT, '/') => 'interactive-schema',
+    //     ];
+    //     $uri = EndpointUtils::getSlashedURI();
+    //     if ($redirectPath = $redirectPaths[trim($uri, '/')]) {
+    //         wp_redirect(GRAPHQL_BY_POP_PLUGIN_URL.$redirectPath);
+    //         die;
+    //     }
+    // }
     public function parseRequest()
     {
-        $redirectPaths = [
-            trim($this->GRAPHIQL_ENDPOINT, '/') => 'graphiql',
-            trim($this->VOYAGER_ENDPOINT, '/') => 'interactive-schema',
-        ];
         $uri = EndpointUtils::getSlashedURI();
-        if ($redirectPath = $redirectPaths[trim($uri, '/')]) {
-            wp_redirect(GRAPHQL_BY_POP_PLUGIN_URL.$redirectPath);
+        $uri = trim($uri, '/');
+        $graphiQLTrimmedEndpoint = trim($this->GRAPHIQL_ENDPOINT, '/');
+        $voyagerTrimmedEndpoint = trim($this->VOYAGER_ENDPOINT, '/');
+        $dirPaths = [
+            $graphiQLTrimmedEndpoint => 'graphiql',
+            $voyagerTrimmedEndpoint => 'interactive-schema',
+        ];
+        if ($dirPath = $dirPaths[$uri]) {
+            $htmlFileNames = [
+                $graphiQLTrimmedEndpoint => 'index.html',
+                $voyagerTrimmedEndpoint => 'index.html',
+            ];
+            // Read the file, and return it already
+            $file = GRAPHQL_BY_POP_PLUGIN_DIR.$dirPath.'/'.$htmlFileNames[$uri];
+            $fileContents = file_get_contents($file, true);
+            // Modify the script path
+            $jsFileNames = [
+                $graphiQLTrimmedEndpoint => 'graphiql.js',
+                $voyagerTrimmedEndpoint => null,
+            ];
+            if ($jsFileName = $jsFileNames[$uri]) {
+                $jsFileURL = GRAPHQL_BY_POP_PLUGIN_URL.$dirPath.'/'.$jsFileName;
+                $fileContents = str_replace($jsFileName, $jsFileURL, $fileContents);
+            }
+            echo $fileContents;
             die;
         }
     }
