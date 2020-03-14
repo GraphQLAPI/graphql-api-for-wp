0f4a65e7f5032c95ddeff308a61a2806a89dd207
Leo <losoviz@gmail.com>
2020-03-14 19:52:43 +0800

    Documented code

--- packages/api-fetch/src/fetch.js
+++ packages/api-fetch/src/fetch.js
@@ -3,15 +3,29 @@
  */
 import fetch from 'isomorphic-fetch';
 
+/**
+ * The endpoint against which to execute GraphQL queries while on the WordPress admin
+ */
+const GRAPHQL_ADMIN_ENDPOINT = '/api/graphql';
+
+/**
+ * Execute a GraphQL Query and return its results
+ *
+ * @param {string} query The GraphQL query to execute
+ * @return {Object} The response from the GraphQL server
+ */
 const fetchGraphQLQuery = (query) => {
 	const content = {
 		query: query,
 	};
-	return fetch( `${ window.location.origin }/api/graphql`, {
+	return fetch( `${ window.location.origin }${ GRAPHQL_ADMIN_ENDPOINT }`, {
 		method: 'post',
 		headers: { 'Content-Type': 'application/json' },
 		body: JSON.stringify( content ),
 	} ).then( ( response ) => response.json() );
 };
 
+/**
+ * Exports
+ */
 export default fetchGraphQLQuery;
