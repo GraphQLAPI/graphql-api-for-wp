c17fefaa725a9e1d45d287eb0ec054724f5eed8d
Leo <losoviz@gmail.com>
2020-03-18 01:53:32 +0800

    Fixed "who can access" message

--- blocks/access-control-list/src/disable-access.js
+++ blocks/access-control-list/src/disable-access.js
@@ -1,10 +1,14 @@
-import { __ } from '@wordpress/i18n';
+import { __, sprintf } from '@wordpress/i18n';
 
 const DisableAccess = ( props ) => {
 	const { className } = props;
 	return (
 		<div className={ className+'__disable_access' }>
-			<p>Saraza barabadanga</p>
+			<p>{ sprintf(
+				'%1$s %2$s',
+				'⛔️',
+				__('Nobody', 'graphql-api')
+			) }</p>
 		</div>
 	);
 }
