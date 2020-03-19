73eedb152c898610a1b672e41192cb56da62b50d
Leo <losoviz@gmail.com>
2020-03-19 13:37:41 +0800

    Add Access Control blocks only in that CPT

--- src/BlockCategories/AccessControlBlockCategory.php
+++ src/BlockCategories/AccessControlBlockCategory.php
@@ -1,6 +1,8 @@
 <?php
 namespace Leoloso\GraphQLByPoPWPPlugin\BlockCategories;
 
+use Leoloso\GraphQLByPoPWPPlugin\PostTypes\GraphQLAccessControlListPostType;
+
 class AccessControlBlockCategory
 {
     public const ACCESS_CONTROL_BLOCK_CATEGORY = 'graphql-api-access-control';
@@ -15,15 +17,29 @@ class AccessControlBlockCategory
         );
     }
 
-    public function getBlockCategories( $categories, $post ) {
-        return array_merge(
-            $categories,
-            array(
+    /**
+     * Register the "Access Control for GraphQL" category when in the Access Control CPT
+     *
+     * @param [type] $categories
+     * @param [type] $post
+     * @return void
+     */
+    public function getBlockCategories( array $categories, $post ) {
+        /**
+         * Only register for the Access Control CPT
+         */
+        if ($post->post_type == GraphQLAccessControlListPostType::POST_TYPE) {
+            return array_merge(
+                $categories,
                 array(
-                    'slug' => self::ACCESS_CONTROL_BLOCK_CATEGORY,
-                    'title' => __( 'Access Control for GraphQL', 'graphql-api' ),
-                ),
-            )
-        );
+                    array(
+                        'slug' => self::ACCESS_CONTROL_BLOCK_CATEGORY,
+                        'title' => __( 'Access Control for GraphQL', 'graphql-api' ),
+                    ),
+                )
+            );
+        }
+
+        return $categories;
     }
 }
