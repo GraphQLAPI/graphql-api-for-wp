c6c88bf70f9b87d39c8c32021101cb6c78a91a91
Leo <losoviz@gmail.com>
2020-03-19 15:02:12 +0800

    setState on user roles

--- blocks/access-control-user-roles/src/user-roles.js
+++ blocks/access-control-user-roles/src/user-roles.js
@@ -4,58 +4,23 @@ import Select from 'react-select';
 
 const UserRoles = ( props ) => {
 	const { className, setAttributes, attributes: { value } } = props;
-	const colourOptions = [
-		{ value: 'ocean', label: 'Ocean', color: '#00B8D9', isFixed: true },
-		{ value: 'blue', label: 'Blue', color: '#0052CC', isDisabled: true },
-		{ value: 'purple', label: 'Purple', color: '#5243AA' },
-		{ value: 'red', label: 'Red', color: '#FF5630', isFixed: true },
-		{ value: 'orange', label: 'Orange', color: '#FF8B00' },
-		{ value: 'yellow', label: 'Yellow', color: '#FFC400' },
-		{ value: 'green', label: 'Green', color: '#36B37E' },
-		{ value: 'forest', label: 'Forest', color: '#00875A' },
-		{ value: 'slate', label: 'Slate', color: '#253858' },
-		{ value: 'silver', label: 'Silver', color: '#666666' },
-	  ];
-	  const groupedOptions = [
-		{
-		  label: 'Colours',
-		  options: colourOptions,
-		},
-		{
-		  label: 'Flavours',
-		  options: flavourOptions,
-		},
-	  ];
-	const flavourOptions = [
-		{ value: 'vanilla', label: 'Vanilla', rating: 'safe' },
-		{ value: 'chocolate', label: 'Chocolate', rating: 'good' },
-		{ value: 'strawberry', label: 'Strawberry', rating: 'wild' },
-		{ value: 'salted-caramel', label: 'Salted Caramel', rating: 'crazy' },
-	  ];
-	const groupStyles = {
-		display: 'flex',
-		alignItems: 'center',
-		justifyContent: 'space-between',
-	  };
-	  const groupBadgeStyles = {
-		backgroundColor: '#EBECF0',
-		borderRadius: '2em',
-		color: '#172B4D',
-		display: 'inline-block',
-		fontSize: 12,
-		fontWeight: 'normal',
-		lineHeight: '1',
-		minWidth: 1,
-		padding: '0.16666666666667em 0.5em',
-		textAlign: 'center',
-	  };
-
-	  const formatGroupLabel = data => (
-		<div style={groupStyles}>
-		  <span>{data.label}</span>
-		  <span style={groupBadgeStyles}>{data.options.length}</span>
-		</div>
-	  );
+	const options = [
+		{ value: 'ocean', label: 'Ocean' },
+		{ value: 'blue', label: 'Blue' },
+		{ value: 'purple', label: 'Purple' },
+		{ value: 'red', label: 'Red' },
+		{ value: 'orange', label: 'Orange' },
+		{ value: 'yellow', label: 'Yellow' },
+		{ value: 'green', label: 'Green' },
+		{ value: 'forest', label: 'Forest' },
+		{ value: 'slate', label: 'Slate' },
+		{ value: 'silver', label: 'Silver' },
+	];
+	/**
+	 * React Select expects an object with this format:
+	 * { value: ..., label: ... },
+	 */
+	const selectedValues = value.map(val => ( {value: val, label: val} ))
 	return (
 		<div className={ className+'__user_roles' }>
 			<Card { ...props }>
@@ -64,11 +29,16 @@ const UserRoles = ( props ) => {
 				</CardHeader>
 				<CardBody>
 					<Select
-						defaultValue={ colourOptions[1] }
-						options={ groupedOptions }
-						formatGroupLabel={ formatGroupLabel }
+						defaultValue={ selectedValues }
+						options={ options }
 						isMulti
 						closeMenuOnSelect={ false }
+						onChange={ selectedOptions =>
+							// Extract the attribute "value"
+							setAttributes( {
+								value: selectedOptions.map(option => option.value)
+							} )
+						}
 					/>
 				</CardBody>
 			</Card>
