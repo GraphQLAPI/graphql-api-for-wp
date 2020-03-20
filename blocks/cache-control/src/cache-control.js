4e593e8fc6c689aaf12d84ec86102e6af1d27334
Leo <losoviz@gmail.com>
2020-03-20 08:31:00 +0800

    Add the max-age text input

--- blocks/cache-control/src/cache-control.js
+++ blocks/cache-control/src/cache-control.js
@@ -1,11 +1,26 @@
 import { __ } from '@wordpress/i18n';
+import { TextControl, Card, CardHeader, CardBody } from '@wordpress/components';
 
 const CacheControl = ( props ) => {
-	const { className } = props;
+	const { className, setAttributes, attributes: { cacheMaxAge } } = props;
 	return (
-		<div className={ className+'__maxage' }>
-			Saraza
-		</div>
+		<Card { ...props }>
+			<CardHeader isShady>{ __('Cache-Control header', 'graphql-api') }</CardHeader>
+			<CardBody>
+				<TextControl
+					label={ __('Max-age (in seconds)', 'graphql-api') }
+					// help={ __('The Cache-Control header will contain the minimum max-age value from all fields/directives involved in the request, or \'no-store\' if the max-age is 0', 'graphql-api') }
+					type="text"
+					value={ cacheMaxAge }
+					className={ className+'__maxage' }
+					onChange={ newValue =>
+						setAttributes( {
+							cacheMaxAge: newValue,
+						} )
+					}
+				/>
+			</CardBody>
+		</Card>
 	);
 }
 
