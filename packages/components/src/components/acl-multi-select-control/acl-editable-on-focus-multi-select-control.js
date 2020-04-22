b4b7d928fa78931a6a19bca3ae76672ab821015b
Leo <losoviz@gmail.com>
2020-04-23 04:46:48 +0800

    Added cursor mouse on hover

--- packages/components/src/components/acl-multi-select-control/acl-editable-on-focus-multi-select-control.js
+++ packages/components/src/components/acl-multi-select-control/acl-editable-on-focus-multi-select-control.js
@@ -11,24 +11,26 @@ import AccessControlListPrintout from './acl-printout';
 
 const AccessControlListEditableOnFocusMultiSelectControl = ( props ) => {
 	const { setAttributes, isSelected, attributes: { accessControlLists }, accessControlComponentClassName } = props;
-	// const className = 'graphql-api-access-control-list-select';
+	const className = 'graphql-api-access-control-list-select';
 	return (
-		<div className={ accessControlComponentClassName }>
-			{ isSelected &&
-				<AccessControlListMultiSelectControl
-					{ ...props }
-					selectedItems={ accessControlLists }
-					setAttributes={ setAttributes }
-					// className={ className }
-				/>
-			}
-			{ !isSelected && (
-				<AccessControlListPrintout
-					{ ...props }
-					selectedItems={ accessControlLists }
-					// className={ className }
-				/>
-			) }
+		<div className={ className }>
+			<div className={ accessControlComponentClassName }>
+				{ isSelected &&
+					<AccessControlListMultiSelectControl
+						{ ...props }
+						selectedItems={ accessControlLists }
+						setAttributes={ setAttributes }
+						className={ className }
+					/>
+				}
+				{ !isSelected && (
+					<AccessControlListPrintout
+						{ ...props }
+						selectedItems={ accessControlLists }
+						className={ className }
+					/>
+				) }
+			</div>
 		</div>
 	);
 }
--- packages/components/src/components/acl-multi-select-control/index.js
+++ packages/components/src/components/acl-multi-select-control/index.js
@@ -1,2 +1,3 @@
+import './style.scss';
 
 export { default as AccessControlListEditableOnFocusMultiSelectControl } from './acl-editable-on-focus-multi-select-control' ;
