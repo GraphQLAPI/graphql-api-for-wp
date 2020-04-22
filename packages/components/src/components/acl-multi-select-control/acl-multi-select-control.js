9798bee6b6b07a5ce63ec64206a0b2c6ebd9b553
Leo <losoviz@gmail.com>
2020-04-22 21:21:56 +0800

    Add undefined elements for user to unselect

--- packages/components/src/components/acl-multi-select-control/acl-multi-select-control.js
+++ packages/components/src/components/acl-multi-select-control/acl-multi-select-control.js
@@ -35,6 +35,24 @@ const AccessControlListMultiSelectControl = compose( [
 			errorMessage: getRetrievingAccessControlListsErrorMessage(),
 		};
 	} ),
+	withSelect( ( select, { items, selectedItems } ) => {
+		/**
+		 * If the selectedItems contain IDs that do not exist in the entries
+		 * (eg: because those objects where deleted) then add them again to the entries,
+		 * so that the user can unselect them (otherwise, they stay in the state forever)
+		 */
+		const itemValues = items.map( item => item.value );
+		const undefinedSelectedItemIDs = selectedItems.filter( selectedItemID => !itemValues.includes(selectedItemID) );
+		return {
+			items: items.concat(undefinedSelectedItemIDs.map( undefinedSelectedItemID => (
+				{
+					group: __('Undefined elements', 'graphql-api'),
+					title: __(`(Undefined element with ID ${ undefinedSelectedItemID })`, 'graphql-api'),
+					value: undefinedSelectedItemID,
+				}
+			) ) )
+		};
+	} ),
 ] )( MultiSelectControl );
 
 export default AccessControlListMultiSelectControl;
