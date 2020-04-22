950e7d337b00911e2d1d2e43c23c1045fe694ad1
Leo <losoviz@gmail.com>
2020-04-22 21:29:50 +0800

    Moved state to own file

--- packages/components/src/components/acl-multi-select-control/acl-multi-select-control.js
+++ packages/components/src/components/acl-multi-select-control/acl-multi-select-control.js
@@ -9,6 +9,7 @@ import { __ } from '@wordpress/i18n';
  * Internal dependencies
  */
 import MultiSelectControl from '../multi-select-control';
+import AddUndefinedSelectedItemIDs from '../multi-select-control/add-undefined-selected-item-ids';
 
 const AccessControlListMultiSelectControl = compose( [
 	withState( { attributeName: 'accessControlLists' } ),
@@ -35,24 +36,7 @@ const AccessControlListMultiSelectControl = compose( [
 			errorMessage: getRetrievingAccessControlListsErrorMessage(),
 		};
 	} ),
-	withSelect( ( select, { items, selectedItems } ) => {
-		/**
-		 * If the selectedItems contain IDs that do not exist in the entries
-		 * (eg: because those objects where deleted) then add them again to the entries,
-		 * so that the user can unselect them (otherwise, they stay in the state forever)
-		 */
-		const itemValues = items.map( item => item.value );
-		const undefinedSelectedItemIDs = selectedItems.filter( selectedItemID => !itemValues.includes(selectedItemID) );
-		return {
-			items: items.concat(undefinedSelectedItemIDs.map( undefinedSelectedItemID => (
-				{
-					group: __('Undefined elements', 'graphql-api'),
-					title: __(`(Undefined element with ID ${ undefinedSelectedItemID })`, 'graphql-api'),
-					value: undefinedSelectedItemID,
-				}
-			) ) )
-		};
-	} ),
+	AddUndefinedSelectedItemIDs,
 ] )( MultiSelectControl );
 
 export default AccessControlListMultiSelectControl;
