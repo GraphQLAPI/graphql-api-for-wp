4e5bffa2f04919fda9996ca916cd46f695ae50d2
Leo <losoviz@gmail.com>
2020-04-22 21:40:22 +0800

    Removed commented code

--- packages/components/src/components/acl-multi-select-control/index.js
+++ packages/components/src/components/acl-multi-select-control/index.js
@@ -1,8 +1,2 @@
-/**
- * Internal dependencies
- */
-// import AccessControlListEditableOnFocusMultiSelectControl from './acl-editable-on-focus-multi-select-control';
-// import './style.scss';
 
-// export default AccessControlListEditableOnFocusMultiSelectControl;
 export { default as AccessControlListEditableOnFocusMultiSelectControl } from './acl-editable-on-focus-multi-select-control' ;
