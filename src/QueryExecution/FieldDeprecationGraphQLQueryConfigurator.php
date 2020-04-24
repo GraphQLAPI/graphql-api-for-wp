f357d3bb0438f0442b49d3b68f9d4e2a8bc00d3c
Leo <losoviz@gmail.com>
2020-04-25 04:07:15 +0800

    Print that field is deprecated in the reason

--- src/QueryExecution/FieldDeprecationGraphQLQueryConfigurator.php
+++ src/QueryExecution/FieldDeprecationGraphQLQueryConfigurator.php
@@ -6,6 +6,7 @@ namespace Leoloso\GraphQLByPoPWPPlugin\QueryExecution;
 
 use Leoloso\GraphQLByPoPWPPlugin\PluginState;
 use PoP\FieldDeprecationByDirective\Facades\FieldDeprecationManagerFacade;
+use PoP\ComponentModel\Facades\Instances\InstanceManagerFacade;
 
 class FieldDeprecationGraphQLQueryConfigurator extends AbstractGraphQLQueryConfigurator
 {
@@ -29,14 +30,26 @@ class FieldDeprecationGraphQLQueryConfigurator extends AbstractGraphQLQueryConfi
                 PluginState::getFieldDeprecationBlock()
             );
             $fieldDeprecationManager = FieldDeprecationManagerFacade::getInstance();
+            $instanceManager = InstanceManagerFacade::getInstance();
             foreach ($fdlBlockItems as $fdlBlockItem) {
                 if ($deprecationReason = $fdlBlockItem['attrs']['deprecationReason']) {
                     // Extract the saved fields
                     if ($typeFields = $fdlBlockItem['attrs']['typeFields']) {
                         if ($entriesForFields = array_filter(
                             array_map(
-                                function ($selectedField) use ($deprecationReason) {
-                                    return $this->getEntryFromField($selectedField, $deprecationReason);
+                                function ($selectedField) use ($instanceManager, $deprecationReason) {
+                                    $entry = $this->getEntryFromField($selectedField, $deprecationReason);
+                                    // Once getting the entry, we an obtain the type and field,
+                                    // and we can modify the deprecated reason in the entry adding this information
+                                    $typeResolverClass = $entry[0];
+                                    $typeResolver = $instanceManager->getInstance($typeResolverClass);
+                                    $entry[2] = sprintf(
+                                        \__('Field \'%1$s\' from type \'%2$s\' has been deprecated: %3$s'),
+                                        $entry[1],
+                                        $typeResolver->getMaybeNamespacedTypeName(),
+                                        $entry[2]
+                                    );
+                                    return $entry;
                                 },
                                 $typeFields
                             )
