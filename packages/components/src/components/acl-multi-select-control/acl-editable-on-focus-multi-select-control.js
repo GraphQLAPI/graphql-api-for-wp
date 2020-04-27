459819bfccd0791050f84415d9118775feb016a3
Leo <losoviz@gmail.com>
2020-04-27 19:22:52 +0800

    Moved code into HOC

--- packages/components/src/components/acl-multi-select-control/acl-editable-on-focus-multi-select-control.js
+++ packages/components/src/components/acl-multi-select-control/acl-editable-on-focus-multi-select-control.js
@@ -9,10 +9,10 @@ import { compose, withState } from '@wordpress/compose';
  */
 import AccessControlListMultiSelectControl from './acl-multi-select-control';
 import AccessControlListPrintout from './acl-printout';
-import { getEditableOnFocusComponentClass } from '../base-styles'
 import { withCard } from '../card'
+import { withEditableOnFocus } from '../editable-on-focus'
 
-const AccessControlListEditableOnFocusMultiSelectControlInner = ( props ) => {
+const AccessControlListEditableOnFocusMultiSelectControl = ( props ) => {
 	const { isSelected, className, attributes: { accessControlLists } } = props;
 	return (
 		<>
@@ -34,23 +34,11 @@ const AccessControlListEditableOnFocusMultiSelectControlInner = ( props ) => {
 	);
 }
 
-const AccessControlListEditableOnFocusMultiSelectControl = ( props ) => {
-	const { isSelected } = props;
-	const className = 'graphql-api-access-control-list-select';
-	const componentClassName = getEditableOnFocusComponentClass(isSelected);
-	return (
-		<div className={ className }>
-			<div className={ componentClassName }>
-				<AccessControlListEditableOnFocusMultiSelectControlInner
-					{ ...props }
-					className={ className }
-				/>
-			</div>
-		</div>
-	);
-}
-
 export default compose( [
-	withState( { header: __('Access Control Lists', 'graphql-api') } ),
-	withCard(),
+	withState( {
+		header: __('Access Control Lists', 'graphql-api'),
+		className: 'graphql-api-access-control-list-select',
+	 } ),
+	 withEditableOnFocus(),
+	 withCard(),
 ] )( AccessControlListEditableOnFocusMultiSelectControl );
