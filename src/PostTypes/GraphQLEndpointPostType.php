4c748d505e67bf2fe37b36c60b475aba3501587c
Leo <losoviz@gmail.com>
2020-04-28 23:33:49 +0800

    Expose GraphiQL/Voyager clients

--- src/PostTypes/GraphQLEndpointPostType.php
+++ src/PostTypes/GraphQLEndpointPostType.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace Leoloso\GraphQLByPoPWPPlugin\PostTypes;
 
+use PoP\ComponentModel\Misc\RequestUtils;
 use Leoloso\GraphQLByPoPWPPlugin\PluginState;
+use Leoloso\GraphQLByPoPWPPlugin\General\RequestParams;
 use Leoloso\GraphQLByPoPWPPlugin\ComponentConfiguration;
 use PoP\GraphQLAPIRequest\Execution\QueryExecutionHelpers;
+use Leoloso\GraphQLByPoPWPPlugin\Blocks\EndpointOptionsBlock;
 use Leoloso\GraphQLByPoPWPPlugin\Taxonomies\GraphQLQueryTaxonomy;
 use Leoloso\GraphQLByPoPWPPlugin\Blocks\AbstractQueryExecutionOptionsBlock;
 use Leoloso\GraphQLByPoPWPPlugin\PostTypes\AbstractGraphQLQueryExecutionPostType;
@@ -163,5 +166,78 @@ class GraphQLEndpointPostType extends AbstractGraphQLQueryExecutionPostType
     protected function getQueryExecutionOptionsBlock(): AbstractQueryExecutionOptionsBlock
     {
         return PluginState::getEndpointOptionsBlock();
+    }
+
+    /**
+     * Indicates if we executing the GraphQL query (`true`) or visualizing the query source (`false`)
+     * It returns always `true`, unless passing ?view=source in the single post URL
+     *
+     * @return boolean
+     */
+    protected function isGraphQLQueryExecution(): bool
+    {
+        return !in_array($_REQUEST[RequestParams::VIEW], ['graphiql', 'schema']);
+    }
+
+    /**
+     * Set the hook to expose the GraphiQL/Voyager clients
+     *
+     * @return void
+     */
+    protected function doSomethingElse(): void
+    {
+        \add_action(
+            'wp',
+            [$this, 'maybePrintClient']
+        );
+    }
+    /**
+     * Expose the GraphiQL/Voyager clients
+     *
+     * @return void
+     */
+    public function maybePrintClient(): void
+    {
+        // Read from the configuration if to expose the GraphiQL/Voyager client
+        $exposeClient = false;
+        $optionsBlockDataItem = $this->getOptionsBlockDataItem();
+        if ($_REQUEST[RequestParams::VIEW] == 'graphiql') {
+            // `true` is the default option in Gutenberg, so it's not saved to the DB!
+            $exposeClient = $optionsBlockDataItem['attrs'][EndpointOptionsBlock::ATTRIBUTE_NAME_IS_GRAPHIQL_ENABLED] ?? true;
+        } elseif ($_REQUEST[RequestParams::VIEW] == 'schema') {
+            $exposeClient = $optionsBlockDataItem['attrs'][EndpointOptionsBlock::ATTRIBUTE_NAME_IS_VOYAGER_ENABLED] ?? true;
+        }
+        if (!$exposeClient) {
+            return;
+        }
+
+        // Read from the static HTML files and replace their endpoints
+        $dirPaths = [
+            'graphiql' => '/vendor/leoloso/pop-graphiql',
+            'schema' => '/vendor/leoloso/pop-graphql-voyager',
+        ];
+        if ($dirPath = $dirPaths[$_REQUEST[RequestParams::VIEW]]) {
+            // Read the file, and return it already
+            $file = \GRAPHQL_BY_POP_PLUGIN_DIR . $dirPath . '/index.html';
+            $fileContents = \file_get_contents($file, true);
+            // Modify the script path
+            $jsFileNames = [
+                'graphiql' => 'graphiql.js',
+                'schema' => 'voyager.js',
+            ];
+            if ($jsFileName = $jsFileNames[$_REQUEST[RequestParams::VIEW]]) {
+                $jsFileURL = \trim(\GRAPHQL_BY_POP_PLUGIN_URL, '/') . $dirPath . '/' . $jsFileName;
+                $useNamespace = '';
+                if (false) {
+                    $useNamespace = '&use_namespace=1';
+                }
+                $endpointURL = \remove_query_arg(RequestParams::VIEW, RequestUtils::getCurrentUrl());
+                $endpointURL .= $useNamespace;
+                $fileContents = \str_replace($jsFileName . '?', $jsFileURL . '?endpoint=' . $endpointURL . '&', $fileContents);
+            }
+            // Print, and that's it!
+            echo $fileContents;
+            die;
+        }
     }
 }
--- src/SchemaConfigurators/PersistedQuerySchemaConfigurator.php
+++ src/SchemaConfigurators/PersistedQuerySchemaConfigurator.php
@@ -7,30 +7,10 @@ namespace Leoloso\GraphQLByPoPWPPlugin\SchemaConfigurators;
 use Leoloso\GraphQLByPoPWPPlugin\PluginState;
 use Leoloso\GraphQLByPoPWPPlugin\General\BlockHelpers;
 use Leoloso\GraphQLByPoPWPPlugin\Blocks\SchemaConfigCacheControlListBlock;
-use Leoloso\GraphQLByPoPWPPlugin\Blocks\PersistedQueryOptionsBlock;
 use Leoloso\GraphQLByPoPWPPlugin\SchemaConfigurators\CacheControlGraphQLQueryConfigurator;
 
 class PersistedQuerySchemaConfigurator extends AbstractQueryExecutionSchemaConfigurator
 {
-    /**
-     * Process the variables
-     *
-     * @return void
-     */
-    protected function executeOptionsSchemaConfiguration(int $customPostID): void
-    {
-        $optionsBlockDataItem = BlockHelpers::getSingleBlockOfTypeFromCustomPost(
-            $customPostID,
-            PluginState::getPersistedQueryOptionsBlock()
-        );
-        // `true` is the default option in Gutenberg, so it's not saved to the DB!
-        $acceptVariablesAsURLParams = $optionsBlockDataItem['attrs'][PersistedQueryOptionsBlock::ATTRIBUTE_NAME_ACCEPT_VARIABLES_AS_URL_PARAMS] ?? true;
-        if ($acceptVariablesAsURLParams) {
-
-        } else {
-
-        }
-    }
     /**
      * Apply all the settings defined in the Schema Configuration:
      * - Access Control Lists
