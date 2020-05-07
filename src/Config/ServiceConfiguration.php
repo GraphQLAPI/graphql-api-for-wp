62ce7f0ff01c9c449314d31869bd90b4fd78e325
Tan Choon Tick <cttan@Tans-MacBook-Air.local>
2020-05-07 18:14:37 +0800

    Use default schemaMode, allowing to cache schema

--- src/Config/ServiceConfiguration.php
+++ src/Config/ServiceConfiguration.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Leoloso\GraphQLByPoPWPPlugin\Config;
 
-use PoP\AccessControl\Schema\SchemaModes;
 use PoP\Engine\TypeResolvers\RootTypeResolver;
 use PoP\Root\Component\PHPServiceConfigurationTrait;
 use PoP\ComponentModel\Container\ContainerBuilderUtils;
@@ -29,9 +28,9 @@ class ServiceConfiguration
             'addEntriesForFields',
             UserRolesAccessControlGroups::CAPABILITIES,
             [
-                [RootTypeResolver::class, 'accessControlLists', $capabilities, SchemaModes::PRIVATE_SCHEMA_MODE],
-                [RootTypeResolver::class, 'cacheControlLists', $capabilities, SchemaModes::PRIVATE_SCHEMA_MODE],
-                [RootTypeResolver::class, 'fieldDeprecationLists', $capabilities, SchemaModes::PRIVATE_SCHEMA_MODE],
+                [RootTypeResolver::class, 'accessControlLists', $capabilities],
+                [RootTypeResolver::class, 'cacheControlLists', $capabilities],
+                [RootTypeResolver::class, 'fieldDeprecationLists', $capabilities],
             ]
         );
     }
