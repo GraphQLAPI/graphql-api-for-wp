8bc9693868e78ab8c241b5ec292a148c3ef6accb
Tan Choon Tick <cttan@Tans-MacBook-Air.local>
2020-05-10 12:08:18 +0800

    Added comments, removed commented code

--- editor-scripts/endpoint-editor-components/src/store/controls.js
+++ editor-scripts/endpoint-editor-components/src/store/controls.js
@@ -1,14 +1,18 @@
 
 /**
- * Execute the GraphQL queries
+ * Receive Markdown files
+ * 
+ * Use webpackMode: "eager" because the module with the translated docs has already been loaded:
+ * @see https://github.com/webpack/webpack/issues/4807#issuecomment-300443876
+ * 
+ * Use { default: _ } for some reason (it doesn't work):
+ * @see https://webpack.js.org/guides/code-splitting/#dynamic-imports
  */
 const controls = {
 	RECEIVE_MARKDOWN_FILES( action ) {
-		// return import( `../markdown/${ action.lang }/${ action.fileName }.md` )
-		// return import( /* webpackChunkName: "guides/[request]" */ `../../guides/${ action.lang }/${ action.fileName }.md` )
 		return import( /* webpackMode: "eager" */ `../../guides/${ action.lang }/${ action.fileName }.md` )
 			.then(obj => obj.default)
-			// .then( { default: _ } )
+			// .then( ( { default: _ } ) )
 			.catch(err => action.defaultValue );
 	},
 };
