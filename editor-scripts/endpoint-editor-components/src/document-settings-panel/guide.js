3cdf9567f31a910ed6260799a4a11f45b5d74fc5
Tan Choon Tick <cttan@Tans-MacBook-Air.local>
2020-05-11 21:29:58 +0800

    Made code more elegant

--- editor-scripts/endpoint-editor-components/src/document-settings-panel/guide.js
+++ editor-scripts/endpoint-editor-components/src/document-settings-panel/guide.js
@@ -4,24 +4,27 @@
 import { useState, useEffect } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { Button, Guide, GuidePage } from '@wordpress/components';
-import {
-	welcomeGuideMarkdown,
-	schemaConfigOptionsMarkdown,
-} from '../../docs';
 
 const EndpointGuide = ( props ) => {
 	const [ pages, setPages ] = useState([]);
-	const lang = 'es'
-	const langSources = {
-		'welcome-guide': welcomeGuideMarkdown,
-		'schema-config-options': schemaConfigOptionsMarkdown,
-	}
+	const lang = 'fr'
+	const defaultLang = 'en'
+	const markdownPageFilenames = [
+		'welcome-guide',
+		'schema-config-options',
+	]
 	useEffect(() => {
-		const importPromises = Object.keys(langSources).map(
-			fileName => import( /* webpackMode: "eager" */ `../../docs/${ lang }/${ fileName }.md` )
+		const getMarkdownContent = ( lang, fileName ) => {
+			return import( /* webpackMode: "eager" */ `../../docs/${ lang }/${ fileName }.md` )
 				.then(obj => obj.default)
 				// .then( ( { default: _ } ) )
-				.catch(err => langSources[ fileName ])
+		}
+		const getMarkdownContentOrUseDefault = ( lang, defaultLang, fileName ) => {
+			return getMarkdownContent( lang, fileName )
+				.catch(err => getMarkdownContent( defaultLang, fileName ) )
+		}
+		const importPromises = markdownPageFilenames.map(
+			fileName => getMarkdownContentOrUseDefault( lang, defaultLang, fileName )
 		)
 		Promise.all(importPromises).then( values => {
 			setPages( values )
