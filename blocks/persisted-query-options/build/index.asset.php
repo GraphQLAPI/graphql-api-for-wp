d1ca8e5daec6ff46303047f8bb8e63f4790a6f66
Tan Choon Tick <cttan@Tans-MacBook-Air.local>
2020-05-12 16:15:10 +0800

    Compiled JS

--- blocks/persisted-query-options/build/index.asset.php
+++ blocks/persisted-query-options/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '3a8e31625a40772cba52bc1d51dd6465');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'b200aefa509643cfe910d7d9c664dd03');
