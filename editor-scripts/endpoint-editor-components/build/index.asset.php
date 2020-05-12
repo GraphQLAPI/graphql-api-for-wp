0003ab6a8c8a6adc09e2c762967244c4f3415329
Tan Choon Tick <cttan@Tans-MacBook-Air.local>
2020-05-12 19:03:21 +0800

    Compiled JS

--- editor-scripts/endpoint-editor-components/build/index.asset.php
+++ editor-scripts/endpoint-editor-components/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'c097cc95b193ccd10ebffc05e8ca90d3');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'a532432dbc7b9810e2ef69a5c154e38b');
