e29b7515f6e744b41b30afb02a4d7bcae1dca981
Leonardo Losoviz <leo@getpop.org>
2020-05-20 19:20:44 +0800

    Initialize components

--- graphql-api.php
+++ graphql-api.php
@@ -29,6 +29,26 @@ require_once(__DIR__ . '/environment.php');
 // Load Composer’s autoloader
 require_once(__DIR__ . '/vendor/autoload.php');
 
+// Initialize all components
+$componentClasses = [
+    \PoP\CommentMetaWP\Component::class,
+    \PoP\GraphQL\Component::class,
+    \PoP\MediaWP\Component::class,
+    \PoP\PagesWP\Component::class,
+    \PoP\PostMediaWP\Component::class,
+    \PoP\PostMetaWP\Component::class,
+    \PoP\TaxonomyQueryWP\Component::class,
+    \PoP\UserRolesAccessControl\Component::class,
+    \PoP\UserRolesWP\Component::class,
+    \PoP\UserStateWP\Component::class,
+    \PoP\UserMetaWP\Component::class,
+    \PoP\UsefulDirectives\Component::class,
+    \PoP\FieldDeprecationByDirective\Component::class,
+];
+foreach ($componentClasses as $componentClass) {
+    $componentClass::initialize();
+}
+
 // Configure the plugin. This defines hooks to set environment variables, so must be executed
 // before those hooks are triggered for first time (in ComponentConfiguration classes)
 \Leoloso\GraphQLByPoPWPPlugin\PluginConfiguration::init();
