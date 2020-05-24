10f0f1fbca1c3a62fa2063efc76b558f5cdb5ec2
Leonardo Losoviz <leo@getpop.org>
2020-05-24 12:34:20 +0800

    Initial tests of afragen/wp-dependency-installer

--- graphql-api.php
+++ graphql-api.php
@@ -43,3 +43,69 @@ $plugin->initialize();
 // Functions to execute when activating/deactivating the plugin
 \register_activation_hook(__FILE__, [$plugin, 'activate']);
 \register_deactivation_hook(__FILE__, [$plugin, 'deactivate']);
+
+/**
+ * Testing https://github.com/afragen/wp-dependency-installe
+ */
+$config = [
+    [
+      'name'    => 'Query Monitor',
+      'host'    => 'wordpress',
+      'slug'    => 'query-monitor/query-monitor.php',
+      'uri'     => 'https://wordpress.org/plugins/query-monitor/',
+      'required' => true,
+    ],
+    // [
+    //   'name'     => 'Debug Bar',
+    //   'host'     => 'wordpress',
+    //   'slug'     => 'debug-bar/debug-bar.php',
+    //   'uri'      => 'https://wordpress.org/plugins/debug-bar/',
+    //   'optional' => true,
+    // ],
+    [
+      'name'     => 'Yoast',
+      'host'     => 'wordpress',
+      'slug'     => 'wordpress-seo/wp-seo.php',
+      'uri'      => 'https://wordpress.org/plugins/wordpress-seo/',
+      'required' => true,
+    ],
+    // [
+    //     'name' => 'WPGraphQL',
+    //     'host' => 'github',
+    //     'slug' => 'wp-graphql/wp-graphql.php',
+    //     'uri' => 'wp-graphql/wp-graphql',
+    //     'branch' => 'master',
+    //     'optional' => true,
+    //     'token' => null,
+    // ],
+    [
+        'name' => 'graphql-api',
+        'host' => 'github',
+        'slug' => 'graphql-api/graphql-api.php',
+        'uri' => 'GraphQLAPI/graphql-api',
+        'branch' => 'master',
+        'required' => true,
+        // 'token' => 'f0a3b0d079796022fc94117ea065f57fafe88bd5',
+    ],
+];
+
+// Disable "Required Plugin" label ( plugin_actions_links )
+\add_filter( 'wp_dependency_required_label', '__return_false' );
+
+// Disable "Required by" row ( plugin_row_meta ).
+\add_filter( 'wp_dependency_required_row_meta', '__return_false' );
+
+// Restore "deactivate" and "delete" links ( plugin_actions_links ).
+\add_filter( 'wp_dependency_unset_action_links', '__return_false' );
+
+\add_filter(
+    'wp_dependency_dismiss_label',
+    function( $label, $source ) {
+        $label = basename(__DIR__) !== $source ? $label : __( 'Group Plugin Installer', 'group-plugin-installer' );
+        return $label;
+    },
+    10,
+2
+);
+
+WP_Dependency_Installer::instance( __DIR__ )->register( $config )->run();
