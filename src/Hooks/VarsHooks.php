185c13f3e336382aeea20322b820628dfc6884df
Leonardo Losoviz <leo@getpop.org>
2020-05-26 04:31:12 +0800

    Remove hook unless single endpoint enabled

--- src/Hooks/VarsHooks.php
+++ src/Hooks/VarsHooks.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace GraphQLAPI\GraphQLAPI\Hooks;
 
+use PoP\Routing\RouteNatures;
 use PoP\Engine\Hooks\AbstractHookSet;
+use GraphQLAPI\GraphQLAPI\Facades\ModuleRegistryFacade;
+use GraphQLAPI\GraphQLAPI\ModuleResolvers\ModuleResolver;
 
 class VarsHooks extends AbstractHookSet
 {
@@ -20,15 +23,19 @@ class VarsHooks extends AbstractHookSet
     }
 
     /**
+     * If the single endpoint is disabled:
      * Do not allow to query the endpoint through URL, like ?scheme=api&datastructure=graphql
      */
     public function maybeRemoveVars($vars_in_array)
     {
-        $vars = &$vars_in_array[0];
-        if ($vars['scheme'] == \POP_SCHEME_API) {
-            // Remove
-            unset($vars['scheme']);
-            unset($vars['datastructure']);
+        $moduleRegistry = ModuleRegistryFacade::getInstance();
+        if (!$moduleRegistry->isModuleEnabled(ModuleResolver::SINGLE_ENDPOINT)) {
+            $vars = &$vars_in_array[0];
+            if ($vars['scheme'] == \POP_SCHEME_API) {
+                // Remove
+                unset($vars['scheme']);
+                unset($vars['datastructure']);
+            }
         }
     }
 }
