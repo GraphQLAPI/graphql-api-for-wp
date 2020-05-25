f14e926544d2e044b564cfc2936836cf99a540cb
Leonardo Losoviz <leo@getpop.org>
2020-05-26 05:51:06 +0800

    Compiled JS

--- blocks/endpoint-options/build/index.asset.php
+++ blocks/endpoint-options/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'a0a5bae661ddb4e8fea7ee692cdfc8a2');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'e016fc3f7f8299639b5bd0ab891aa2dd');
