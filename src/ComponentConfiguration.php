8d5d1507898efed6b138e708ddbf0471e7f96779
Leonardo Losoviz <leo@getpop.org>
2020-05-27 14:36:42 +0800

    Simplified setting configuration values

--- src/ComponentConfiguration.php
+++ src/ComponentConfiguration.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace GraphQLAPI\GraphQLAPI;
 
+use GraphQLAPI\GraphQLAPI\Security\UserAuthorization;
+use PoP\ComponentModel\ComponentConfiguration\EnvironmentValueHelpers;
 use PoP\ComponentModel\ComponentConfiguration\ComponentConfigurationTrait;
 
 class ComponentConfiguration
@@ -20,145 +22,200 @@ class ComponentConfiguration
     private static $getSchemaEditorAccessScheme;
     private static $enableLowLevelSchemaEditing;
 
+    /**
+     * Print the excerpt as description in the custom post types
+     *
+     * @return boolean
+     */
     public static function addExcerptAsDescription(): bool
     {
         // Define properties
         $envVariable = Environment::ADD_EXCERPT_AS_DESCRIPTION;
         $selfProperty = &self::$addExcerptAsDescription;
-        $callback = [Environment::class, 'addExcerptAsDescription'];
+        $defaultValue = true;
+        $callback = [EnvironmentValueHelpers::class, 'toBool'];
 
         // Initialize property from the environment/hook
         self::maybeInitializeConfigurationValue(
             $envVariable,
             $selfProperty,
+            $defaultValue,
             $callback
         );
         return $selfProperty;
     }
 
+    /**
+     * Group the fields under the type when printing it for the user
+     *
+     * @return boolean
+     */
     public static function groupFieldsUnderTypeForPrint(): bool
     {
         // Define properties
         $envVariable = Environment::GROUP_FIELDS_UNDER_TYPE_FOR_PRINT;
         $selfProperty = &self::$groupFieldsUnderTypeForPrint;
-        $callback = [Environment::class, 'groupFieldsUnderTypeForPrint'];
+        $defaultValue = true;
+        $callback = [EnvironmentValueHelpers::class, 'toBool'];
 
         // Initialize property from the environment/hook
         self::maybeInitializeConfigurationValue(
             $envVariable,
             $selfProperty,
+            $defaultValue,
             $callback
         );
         return $selfProperty;
     }
 
+    /**
+     * The label to show when the value is empty
+     *
+     * @return boolean
+     */
     public static function getEmptyLabel(): string
     {
         // Define properties
         $envVariable = Environment::EMPTY_LABEL;
         $selfProperty = &self::$getEmptyLabel;
-        $callback = [Environment::class, 'getEmptyLabel'];
+        $defaultValue = \__('---', 'graphql-api');
 
         // Initialize property from the environment/hook
         self::maybeInitializeConfigurationValue(
             $envVariable,
             $selfProperty,
-            $callback
+            $defaultValue
         );
         return $selfProperty;
     }
 
+    /**
+     * The label to show when the value comes from the settings
+     *
+     * @return boolean
+     */
     public static function getSettingsValueLabel(): string
     {
         // Define properties
         $envVariable = Environment::SETTINGS_VALUE_LABEL;
         $selfProperty = &self::$getSettingsValueLabel;
-        $callback = [Environment::class, 'getSettingsValueLabel'];
+        $defaultValue = \__('As defined in the General Settings', 'graphql-api');
 
         // Initialize property from the environment/hook
         self::maybeInitializeConfigurationValue(
             $envVariable,
             $selfProperty,
-            $callback
+            $defaultValue
         );
         return $selfProperty;
     }
 
+    /**
+     * Use GraphiQL with the Explorer
+     *
+     * @return boolean
+     */
     public static function useGraphiQLWithExplorer(): bool
     {
         // Define properties
         $envVariable = Environment::USE_GRAPHIQL_WITH_EXPLORER;
         $selfProperty = &self::$useGraphiQLWithExplorer;
-        $callback = [Environment::class, 'useGraphiQLWithExplorer'];
+        $defaultValue = true;
+        $callback = [EnvironmentValueHelpers::class, 'toBool'];
 
         // Initialize property from the environment/hook
         self::maybeInitializeConfigurationValue(
             $envVariable,
             $selfProperty,
+            $defaultValue,
             $callback
         );
         return $selfProperty;
     }
 
+    /**
+     * The slug to use as base when accessing the endpoint
+     *
+     * @return string
+     */
     public static function getEndpointSlugBase(): string
     {
         // Define properties
         $envVariable = Environment::ENDPOINT_SLUG_BASE;
         $selfProperty = &self::$getEndpointSlugBase;
-        $callback = [Environment::class, 'getEndpointSlugBase'];
+        $defaultValue = 'graphql';
 
         // Initialize property from the environment/hook
         self::maybeInitializeConfigurationValue(
             $envVariable,
             $selfProperty,
-            $callback
+            $defaultValue
         );
         return $selfProperty;
     }
 
+    /**
+     * The slug to use as base when accessing the persisted query
+     *
+     * @return string
+     */
     public static function getPersistedQuerySlugBase(): string
     {
         // Define properties
         $envVariable = Environment::PERSISTED_QUERY_SLUG_BASE;
         $selfProperty = &self::$getPersistedQuerySlugBase;
-        $callback = [Environment::class, 'getPersistedQuerySlugBase'];
+        $defaultValue = 'graphql-query';
 
         // Initialize property from the environment/hook
         self::maybeInitializeConfigurationValue(
             $envVariable,
             $selfProperty,
-            $callback
+            $defaultValue
         );
         return $selfProperty;
     }
 
+    /**
+     * If `"admin"`, only the admin can compose a GraphQL query and endpoint
+     * If `"post"`, the workflow from creating posts is employed (i.e. Author role can create
+     * but not publish the query, Editor role can publish it, etc)
+     *
+     * @return string
+     */
     public static function getSchemaEditorAccessScheme(): ?string
     {
         // Define properties
         $envVariable = Environment::SCHEMA_EDITOR_ACCESS_SCHEME;
         $selfProperty = &self::$getSchemaEditorAccessScheme;
-        $callback = [Environment::class, 'getSchemaEditorAccessScheme'];
+        $defaultValue = UserAuthorization::ACCESS_SCHEME_ADMIN_ONLY;
 
         // Initialize property from the environment/hook
         self::maybeInitializeConfigurationValue(
             $envVariable,
             $selfProperty,
-            $callback
+            $defaultValue
         );
         return $selfProperty;
     }
 
+    /**
+     * If `true`, it makes Schema-type directives available in the GraphiQL editor
+     *
+     * @return boolean
+     */
     public static function enableLowLevelSchemaEditing(): bool
     {
         // Define properties
         $envVariable = Environment::ENABLE_LOW_LEVEL_SCHEMA_EDITING;
         $selfProperty = &self::$enableLowLevelSchemaEditing;
-        $callback = [Environment::class, 'enableLowLevelSchemaEditing'];
+        $defaultValue = false;
+        $callback = [EnvironmentValueHelpers::class, 'toBool'];
 
         // Initialize property from the environment/hook
         self::maybeInitializeConfigurationValue(
             $envVariable,
             $selfProperty,
+            $defaultValue,
             $callback
         );
         return $selfProperty;
