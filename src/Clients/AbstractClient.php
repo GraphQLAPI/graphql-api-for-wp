1a5d20efb69a3cfd97c205e53adddcfbe5e23434
Leonardo Losoviz <leo@getpop.org>
2020-05-27 22:31:35 +0800

    Split class into Endpoint/Client

--- src/Clients/AbstractClient.php
+++ src/Clients/AbstractClient.php
@@ -8,21 +8,8 @@ use PoP\APIEndpoints\EndpointUtils;
 use PoP\ComponentModel\ComponentConfiguration as ComponentModelComponentConfiguration;
 use PoP\API\Configuration\Request;
 
-abstract class AbstractClient
+abstract class AbstractClient extends AbstractEndpointHandler
 {
-    /**
-     * Endpoint
-     *
-     * @var string
-     */
-    protected $endpoint;
-
-    /**
-     * Provide the endpoint
-     *
-     * @var string
-     */
-    abstract protected function getEndpoint(): string;
 
     /**
      * Initialize the client
@@ -31,27 +18,14 @@ abstract class AbstractClient
      */
     public function initialize(): void
     {
+        parent::initialize();
+
         /**
          * Subject to the endpoint having been defined
          */
         if ($this->endpoint = $this->getEndpoint()) {
-            // Make sure the endpoint has trailing "/" on both ends
-            $this->endpoint = EndpointUtils::slashURI($this->endpoint);
             /**
-             * Register the endpoints
-             */
-            \add_action(
-                'init',
-                [$this, 'addRewriteEndpoints']
-            );
-            \add_filter(
-                'query_vars',
-                [$this, 'addQueryVar'],
-                10,
-                1
-            );
-            /**
-             * Process the request to find out if it is any of the endpoints
+             * Print the Client HTML
              */
             \add_action(
                 'parse_request',
@@ -131,18 +105,6 @@ abstract class AbstractClient
     }
 
     /**
-     * Indicate if the endpoint has been requested
-     *
-     * @return void
-     */
-    protected function isEndpointRequested(): bool
-    {
-        // Check if the URL ends with either /api/graphql/ or /api/rest/ or /api/
-        $uri = EndpointUtils::removeMarkersFromURI($_SERVER['REQUEST_URI']);
-        return EndpointUtils::doesURIEndWith($uri, $this->endpoint);
-    }
-
-    /**
      * If the endpoint for the client is requested, print the client and exit
      *
      * @return void
@@ -153,45 +115,5 @@ abstract class AbstractClient
             echo $this->getClientHTML();
             die;
         }
-    }
-
-    /**
-     * If use full permalink, the endpoint must be the whole URL.
-     * Otherwise, it can be attached at the end of some other URI (eg: a custom post)
-     *
-     * @return boolean
-     */
-    protected function useFullPermalink(): bool
-    {
-        return false;
-    }
-
-    /**
-     * Add the endpoints to WordPress
-     *
-     * @return void
-     */
-    public function addRewriteEndpoints()
-    {
-        /**
-         * The mask indicates where to apply the endpoint rewriting
-         * @see https://codex.wordpress.org/Rewrite_API/add_rewrite_endpoint
-         */
-        $mask = $this->useFullPermalink() ? constant('EP_ROOT') : constant('EP_ALL');
-
-        // The endpoint passed to `add_rewrite_endpoint` cannot have "/" on either end, or it doesn't work
-        \add_rewrite_endpoint(trim($this->endpoint, '/'), $mask);
-    }
-
-    /**
-     * Add the endpoint query vars
-     *
-     * @param array $query_vars
-     * @return void
-     */
-    public function addQueryVar($query_vars)
-    {
-        $query_vars[] = $this->endpoint;
-        return $query_vars;
     }
 }
