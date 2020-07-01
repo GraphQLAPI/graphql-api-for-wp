0db2d45b4925a1ac0e4c0c36bf9809d09b625134
Mac <mac@Macs-MacBook-Pro.local>
2020-07-01 12:32:32 +0800

    Automate reading all folders

--- dev-helpers/scripts/create-node-modules-symlinks.sh
+++ dev-helpers/scripts/create-node-modules-symlinks.sh
@@ -3,24 +3,38 @@
 # to all the blocks/editor-scripts/packages in the plugin
 # Then, a single node_modules/ folder can service everything
 # Make sure package.json contains ALL dependencies needed for everything
+
+# Current directory
 DIR="$( dirname ${BASH_SOURCE[0]} )"
+
+# node_modules/ directory, under packages/
 NODE_MODULES_DIR="$DIR/../packages/node_modules/"
-cd "$DIR"
+
+# Function `createSymlinks` will create a 'node_modules/' symlink
+# for all folders in the current directory
+createSymlinks(){
+    CURRENT_DIR=$( pwd )
+    echo "In folder '$CURRENT_DIR'"
+    for file in ./*
+    do
+        # Make sure it is a directory
+        if [ -d "$file" ]; then
+            echo "Maybe creating symlink for '$file'"
+            # Create symlink
+            ln -snf "$NODE_MODULES_DIR" "$file"
+        fi
+    done
+}
+
 
 # Blocks
-declare -a BlockArray=("access-control" "access-control-disable-access" "access-control-user-capabilities" "access-control-user-roles" "access-control-user-state" "cache-control" "endpoint-options" "field-deprecation" "graphiql" "graphiql-with-explorer" "persisted-query-options" "schema-config-access-control-lists" "schema-config-cache-control-lists" "schema-config-field-deprecation-lists" "schema-config-options" "schema-configuration")
-for val in ${BlockArray[@]}; do
-   ln -snf $NODE_MODULES_DIR "../../blocks/$val/"
-done
+cd "$DIR/../../blocks/"
+createSymlinks
 
 # Editor Scripts
-declare -a EditorScriptArray=("endpoint-editor-components" "persisted-query-editor-components")
-for val in ${EditorScriptArray[@]}; do
-   ln -snf $NODE_MODULES_DIR "../../editor-scripts/$val/"
-done
+cd "$DIR/../../editor-scripts/"
+createSymlinks
 
 # Packages
-declare -a PackageArray=("api-fetch" "components")
-for val in ${PackageArray[@]}; do
-   ln -snf $NODE_MODULES_DIR "../../packages/$val/"
-done
+cd "$DIR/../../packages/"
+createSymlinks
