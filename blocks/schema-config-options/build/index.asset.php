9a3580c9c9e91c0a86aec1649bd5fa7f9264be0c
Mac <mac@Macs-MacBook-Pro.local>
2020-07-07 02:29:34 +0800

    Compiled using `npm start`

--- blocks/schema-config-options/build/index.asset.php
+++ blocks/schema-config-options/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'd874aef136a5f6bcffe46ce01257593d');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '6571f9c73ab9d91fb405f142817611e7');
