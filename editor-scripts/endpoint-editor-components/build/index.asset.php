a1cc54498d4a0ea44ba2e300f20f2a7275fee4ca
Leonardo Losoviz <leo@getpop.org>
2020-07-10 18:25:50 +0800

    Compiled JS

--- editor-scripts/endpoint-editor-components/build/index.asset.php
+++ editor-scripts/endpoint-editor-components/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-primitives'), 'version' => 'b1611f19da66e032f881a41a67ea6482');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-primitives'), 'version' => 'd0fe5ba8af22d0d388b05de3bb21292a');
