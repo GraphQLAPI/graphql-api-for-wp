7589e78356e2a090613ee0c8ae7b04a266d0eb91
Leonardo Losoviz <leo@getpop.org>
2020-07-14 08:25:24 +0800

    Changed enum to uppercase

--- packages/components/src/constants/directive-types.js
+++ packages/components/src/constants/directive-types.js
@@ -1,10 +1,12 @@
 /**
- * Same value as in PoP\ComponentModel\Directives\DirectiveTypes::SCHEMA
+ * Same value as in PoP\ComponentModel\Directives\DirectiveTypes::SCHEMA,
+ * but converted to uppercase as done by PoP\ComponentModel\Enums\DirectiveTypeEnum
  */
-const DIRECTIVE_TYPE_SCHEMA = 'schema';
+const DIRECTIVE_TYPE_SCHEMA = 'SCHEMA';
 /**
- * Same value as in PoP\ComponentModel\Directives\DirectiveTypes::QUERY
+ * Same value as in PoP\ComponentModel\Directives\DirectiveTypes::QUERY,
+ * but converted to uppercase as done by PoP\ComponentModel\Enums\DirectiveTypeEnum
  */
-const DIRECTIVE_TYPE_QUERY = 'query';
+const DIRECTIVE_TYPE_QUERY = 'QUERY';
 
 export { DIRECTIVE_TYPE_SCHEMA, DIRECTIVE_TYPE_QUERY };
