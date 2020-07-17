f8de7bdd38466ec3a5d2c78fc04499fa0e250a39
Leonardo Losoviz <leo@getpop.org>
2020-07-17 14:50:22 +0800

    Removed "@" from directives as label

--- src/Blocks/AbstractControlBlock.php
+++ src/Blocks/AbstractControlBlock.php
@@ -84,10 +84,12 @@ abstract class AbstractControlBlock extends AbstractBlock
             $directiveContent = ComponentConfiguration::getEmptyLabel();
             $directives = $attributes[self::ATTRIBUTE_NAME_DIRECTIVES] ?? [];
             if ($directives) {
-                // Notice we are adding the "@" symbol for GraphQL directives
+                // // Notice we are adding the "@" symbol for GraphQL directives
                 $directiveContent = sprintf(
-                    '<ul><li><code>@%s</code></li></ul>',
-                    implode('</code></li><li><code>@', $directives)
+                    // '<ul><li><code>@%s</code></li></ul>',
+                    // implode('</code></li><li><code>@', $directives)
+                    '<ul><li><code>%s</code></li></ul>',
+                    implode('</code></li><li><code>', $directives)
                 );
             }
         }
