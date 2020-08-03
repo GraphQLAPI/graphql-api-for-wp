82e0fd7bfcfb212d6aa2ea6837c857ee6d49533b
Leonardo Losoviz <leo@getpop.org>
2020-08-04 01:26:18 +0800

    Fixed best practice by Scrutinizer

--- src/General/EditorHelpers.php
+++ src/General/EditorHelpers.php
@@ -24,11 +24,13 @@ class EditorHelpers
         if (!in_array($pagenow, ['post-new.php', 'post.php'])) {
             return null;
         }
+        $typenow = null;
         if ('post-new.php' === $pagenow) {
             if (isset($_REQUEST['post_type']) && \post_type_exists($_REQUEST['post_type'])) {
                 $typenow = $_REQUEST['post_type'];
             };
         } elseif ('post.php' === $pagenow) {
+            $post_id = null;
             if (isset($_GET['post']) && isset($_POST['post_ID']) && (int) $_GET['post'] !== (int) $_POST['post_ID']) {
                 // Do nothing
             } elseif (isset($_GET['post'])) {
@@ -36,7 +38,7 @@ class EditorHelpers
             } elseif (isset($_POST['post_ID'])) {
                 $post_id = (int) $_POST['post_ID'];
             }
-            if ($post_id) {
+            if (!is_null($post_id)) {
                 $post = \get_post($post_id);
                 $typenow = $post->post_type;
             }
