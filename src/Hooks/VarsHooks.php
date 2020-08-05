4c3b7eb28798180000c7cd2f9eaf7076b9c04d4b
Leonardo Losoviz <leo@getpop.org>
2020-08-05 22:52:05 +0800

    Avoid querying the native API through URL

--- src/Hooks/VarsHooks.php
+++ src/Hooks/VarsHooks.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace GraphQLAPI\GraphQLAPI\Hooks;
 
-use PoP\Routing\RouteNatures;
 use PoP\Engine\Hooks\AbstractHookSet;
 use GraphQLAPI\GraphQLAPI\Facades\ModuleRegistryFacade;
 use GraphQLAPI\GraphQLAPI\ModuleResolvers\FunctionalityModuleResolver;
+use PoP\GraphQLAPI\DataStructureFormatters\GraphQLDataStructureFormatter;
 
 class VarsHooks extends AbstractHookSet
 {
@@ -23,16 +23,32 @@ class VarsHooks extends AbstractHookSet
     }
 
     /**
-     * If the single endpoint is disabled:
-     * Do not allow to query the endpoint through URL, like ?scheme=api&datastructure=graphql
+     * If the single endpoint is disabled, or if pointing to a different URL
+     * than the single endpoint (eg: /posts/) and the datastructure param
+     * is not provided it's not "graphql", then:
+     * Do not allow to query the endpoint through URL.
+     *
+     * Examples of not allowed URLs:
+     * - /single-endpoint/?scheme=api&datastructure=graphql <= single endpoint disabled
+     * - /posts/?scheme=api
      */
     public function maybeRemoveVars($vars_in_array)
     {
-        $moduleRegistry = ModuleRegistryFacade::getInstance();
-        if (!$moduleRegistry->isModuleEnabled(FunctionalityModuleResolver::SINGLE_ENDPOINT)) {
-            $vars = &$vars_in_array[0];
-            if ($vars['scheme'] == \POP_SCHEME_API) {
-                // Remove
+        $vars = &$vars_in_array[0];
+        if ($vars['scheme'] == \POP_SCHEME_API) {
+            $moduleRegistry = ModuleRegistryFacade::getInstance();
+            // By setting explicit allowed datastructures, we avoid the empty one
+            // being processed /?scheme=api <= native API
+            // If ever need to support REST or another format, add a hook here
+            $allowedDataStructures = [
+                GraphQLDataStructureFormatter::getName(),
+            ];
+            if (
+                // If single endpoint not enabled
+                !$moduleRegistry->isModuleEnabled(FunctionalityModuleResolver::SINGLE_ENDPOINT)
+                // If datastructure is not GraphQL (or another allowed one)
+                || !in_array($vars['datastructure'], $allowedDataStructures)
+            ) {
                 unset($vars['scheme']);
                 unset($vars['datastructure']);
             }
