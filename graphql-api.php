3417e7f8b24117521c0360d6175653fc85b2fb9c
Leonardo Losoviz <leo@getpop.org>
2020-08-26 18:25:01 +0800

    Deal with plugin being installed more than once

--- graphql-api.php
+++ graphql-api.php
@@ -19,10 +19,31 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
+/**
+ * Make sure this plugin is not duplicated.
+ * For instance, if zip file already exists in Downloads folder, then
+ * the newly downloaded file will be renamed (eg: graphql-api(2).zip)
+ * and the plugin will exist twice, as graphql-api/... and graphql-api2/...
+ */
+if (defined('GRAPHQL_API_VERSION')) {
+    \add_action('admin_notices', function () {
+        _e(sprintf(
+            '<div class="notice notice-error">' .
+                '<p>%s</p>' .
+            '</div>',
+            sprintf(
+                __('Plugin GraphQL API for WordPress is already installed with version <code>%s</code>, so version <code>%s</code> has not been loaded. Please keep only the latest version of the plugin, remove the other one.', 'graphql-api'),
+                \GRAPHQL_API_VERSION,
+                '0.4'
+            )
+        ));
+    });
+    return;
+}
+define('GRAPHQL_API_VERSION', '0.4');
 define('GRAPHQL_API_PLUGIN_FILE', __FILE__);
 define('GRAPHQL_API_DIR', dirname(__FILE__));
 define('GRAPHQL_API_URL', plugin_dir_url(__FILE__));
-define('GRAPHQL_API_VERSION', '0.4');
 
 // Load Composer’s autoloader
 require_once(__DIR__ . '/vendor/autoload.php');
