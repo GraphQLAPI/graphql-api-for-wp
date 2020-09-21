295a3863d487771d9fd9234fa0d13d914d5f4ffa
Leonardo Losoviz <leo@getpop.org>
2020-09-21 23:29:18 +0800

    Sanitize filename

--- src/Admin/MenuPages/AbstractDocAboutMenuPage.php
+++ src/Admin/MenuPages/AbstractDocAboutMenuPage.php
@@ -18,9 +18,34 @@ abstract class AbstractDocAboutMenuPage extends AbstractDocsMenuPage
         return true;
     }
 
+    /**
+     * Enable "/" in the filename
+     *
+     * @param string[] $specialChars
+     * @return string[]
+     */
+    public function enableSpecialCharsForSanitization(array $specialChars): array
+    {
+        return array_diff(
+            $specialChars,
+            [
+                '/',
+            ]
+        );
+    }
+
     protected function getContentToPrint(): string
     {
-        $doc = $_REQUEST[RequestParams::DOC] . '.md';
+        // Enable "/" in the filename
+        add_filter(
+            'sanitize_file_name_chars',
+            [$this, 'enableSpecialCharsForSanitization']
+        );
+        $doc = \sanitize_file_name($_REQUEST[RequestParams::DOC]) . '.md';
+        remove_filter(
+            'sanitize_file_name_chars',
+            [$this, 'enableSpecialCharsForSanitization']
+        );
         $markdownContentParser = MarkdownContentParserFacade::getInstance();
         try {
             return $markdownContentParser->getContent($doc);
