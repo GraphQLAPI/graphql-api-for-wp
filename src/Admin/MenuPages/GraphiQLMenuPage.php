61f58a0093147ad17503fd267a46b556e8224bb0
Leonardo Losoviz <leo@getpop.org>
2020-11-01 15:31:13 +0800

    Use PHP 7.3 Flexible Heredoc syntax

--- src/Admin/MenuPages/GraphiQLMenuPage.php
+++ src/Admin/MenuPages/GraphiQLMenuPage.php
@@ -207,51 +207,51 @@ class GraphiQLMenuPage extends AbstractMenuPage
     protected function getDefaultQuery(): string
     {
         return <<<EOT
-# Welcome to GraphiQL
-#
-# GraphiQL is an in-browser tool for writing, validating, and
-# testing GraphQL queries.
-#
-# Type queries into this side of the screen, and you will see intelligent
-# typeaheads aware of the current GraphQL type schema and live syntax and
-# validation errors highlighted within the text.
-#
-# GraphQL queries typically start with a "{" character. Lines that starts
-# with a # are ignored.
-#
-# An example GraphQL query might look like:
-#
-#   {
-#     field(arg: "value") {
-#       subField
-#     }
-#   }
-#
-# Run the query (at any moment):
-#
-#   Ctrl-Enter (or press the play button above)
-#
+            # Welcome to GraphiQL
+            #
+            # GraphiQL is an in-browser tool for writing, validating, and
+            # testing GraphQL queries.
+            #
+            # Type queries into this side of the screen, and you will see intelligent
+            # typeaheads aware of the current GraphQL type schema and live syntax and
+            # validation errors highlighted within the text.
+            #
+            # GraphQL queries typically start with a "{" character. Lines that starts
+            # with a # are ignored.
+            #
+            # An example GraphQL query might look like:
+            #
+            #   {
+            #     field(arg: "value") {
+            #       subField
+            #     }
+            #   }
+            #
+            # Run the query (at any moment):
+            #
+            #   Ctrl-Enter (or press the play button above)
+            #
 
-query {
-  posts(limit:3) {
-    id
-    title
-    date(format:"d/m/Y")
-    url
-    author {
-      id
-      name
-      url
-    }
-    tags {
-      name
-    }
-    featuredImage {
-      src
-    }
-  }
-}
+            query {
+              posts(limit:3) {
+                id
+                title
+                date(format:"d/m/Y")
+                url
+                author {
+                  id
+                  name
+                  url
+                }
+                tags {
+                  name
+                }
+                featuredImage {
+                  src
+                }
+              }
+            }
 
-EOT;
+            EOT;
     }
 }
