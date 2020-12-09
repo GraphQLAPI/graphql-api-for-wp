100f27bafd4a5bd975717ffce7afb16632cbfa18
Leonardo Losoviz <leo@getpop.org>
2020-12-09 20:52:17 +0800

    Fixed style

--- src/PostTypes/AbstractPostType.php
+++ src/PostTypes/AbstractPostType.php
@@ -237,7 +237,7 @@ abstract class AbstractPostType
                         $this->getAlignClass(),
                         $excerpt
                     ) . $content;
-                    }
+                }
             }
         }
         return $content;
