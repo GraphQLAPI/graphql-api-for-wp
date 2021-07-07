3d47d49fc465ebafe4f0038ff5f95d9cbebcc892
leoloso <leo@getpop.org>
2021-07-07 00:27:30 +0000

    Introduced Properties::CAN_BE_EMPTY

--- src/Services/Helpers/LocaleHelper.php
+++ src/Services/Helpers/LocaleHelper.php
@@ -4,15 +4,15 @@ declare(strict_types=1);
 
 namespace GraphQLAPI\GraphQLAPI\Services\Helpers;
 
-class LocaleUtils
+use GraphQLAPI\GraphQLAPI\Static\LocaleUtils;
+
+class LocaleHelper
 {
     /**
      * User's selected language code for the admin panel
      */
     public function getLocaleLanguage(): string
     {
-        // locale has shape "en_US". Retrieve the language code only: "en"
-        $localeParts = explode('_', \get_locale());
-        return $localeParts[0];
+        return LocaleUtils::getLocaleLanguage();
     }
 }
